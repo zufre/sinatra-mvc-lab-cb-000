ecf117e72dc1d7d6c8995021960477d197f8a602
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-19 20:55:59 -0500

    unit tests

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,7 @@
 class PigLatinizer
 
   def piglatinize(word)
+
     non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
     vowels = ["a", "e", "i", "o", "u"]
 
@@ -25,3 +26,6 @@ class PigLatinizer
 
 end
 
+p = PigLatinizer.new
+
+puts p.piglatinize("pork")
