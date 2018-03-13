78049befad6c128834eb8a4a5a74f00dc9d2ad71
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-03-13 11:05:35 -0400

    fixed solution to match new improved testing (single method expected from students)

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.to_pig_latin(params[:user_phrase])
+    @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
   end
 
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,18 @@
 class PigLatinizer
 
-  def piglatinize(word)
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
     # word starts with vowel
     if !consonant?(word[0])
       word = word + "w"
@@ -17,13 +29,11 @@ class PigLatinizer
     word << "ay"
   end
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
 
-  def to_pig_latin(sentence)
-    sentence.split.collect { |word| piglatinize(word) }.join(" ")
-  end
+
+
 
 end
-
