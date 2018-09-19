6bc846d71905e63fa94aa5e234f1a5de67c8204c
Cedric David <cedric.david@btopenworld.com>
2018-09-19 23:43:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
 
 
-    @text = Piglatinizer.new.piglatinize("#{params[:user_phrase]}")
+    @text = PigLatinizer.new.piglatinize("#{params[:user_phrase]}")
 
     erb :result
 end
